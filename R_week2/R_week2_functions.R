f373e47e5f9599a5a26df72fa64931eba6510569
Cristian Bidea <cristi@king.com>
2015-03-09 14:59:39 +0200

    functions

--- R_week2/R_week2_functions.R
+++ R_week2/R_week2_functions.R
@@ -8,11 +8,11 @@ print (x)
 
 columnmean <- function(y, removeNA=TRUE)
 {
-    nc <- ncol(y)
-    means <- numeric(nc)
-    for (i in 1:nc)
-    {
-        means[i] <- mean(y[,i], rm.na = removeNA)
-    }
-    means
+  nc <- ncol(y)
+  means <- numeric(nc)
+  for (i in 1:nc)
+  {
+    means[i] <- mean(y[,i], rm.na = removeNA)
+  }
+  means
 }
